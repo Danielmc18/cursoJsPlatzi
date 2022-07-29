bbffdcde2bd05e0c48b8030ec6cf6ab62a2be899
Daniel <daniel.montemayorcarlin@hotmail.com>
2022-07-28 21:08:44 -0500

    Archivo sobre Variables en Js

---
+++ variables.js
@@ -0,0 +1,24 @@
+// Representacion de un lugar en memoria que puede cambiar
+
+let nombre = "Daniel";
+
+// Declarar
+let edad;
+
+// iniciallizar
+edad = 30;
+
+edad
+
+let elementos = ["Computadora","Celular"];
+
+elementos[0] = "computadora"
+elementos[1] = "celular"
+
+let persona = {
+    nombre: "daniel",
+    edad: 20,
+    telefono: 8114743575
+}
+
+persona
