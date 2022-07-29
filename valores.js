3b840286bb16ac5e8374c68c1c827394d8dfaec0
Daniel <daniel.montemayorcarlin@hotmail.com>
2022-07-28 20:56:12 -0500

    Primer Archivo

---
+++ valores.js
@@ -0,0 +1,22 @@
+//Valores Primitivos
+
+//Numeros
+40
+
+//String
+"Daniel Montemayor Carlin"
+
+//Booleanos
+true
+false
+
+//Valores Vacios --> Valores PlaceHolder --> Espacio reservado
+null
+undefined
+
+//Valores No-Primitivos o Arreglo
+
+[1,2,3] 
+
+{ nombre: "Daniel"}
+
